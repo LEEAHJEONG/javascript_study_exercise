b241f5c0052781d22427817920669883aa9000f7
lha901222 <lha901222@gmail.com>
2024-04-09 11:52:26 +0900

    연습

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
-let a = 3,
-  b = "3",
-  c = 5,
-  d = 3;
+// 원의 넓이 구하기
+let r = 8;
+let area;
 
-console.log(a == b); // true
-console.log(a === b); // false
-console.log(a != b); // false a랑 b랑 다르냐
-console.log(a !== b); // true a랑 b랑 데이터타입도 같냐
-console.log(a > c); // f
-console.log(a < c); // t
-console.log(a >= c); // f
-console.log(a <= c); // t
+const pi = 3.14;
+area = r * r * pi;
+console.log(`반지름: ${r}`);
+console.log(`원의 넓이: ${area}`);
